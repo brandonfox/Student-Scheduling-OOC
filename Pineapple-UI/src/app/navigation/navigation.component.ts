c59b20b90d60f779ea169de23cc423fb310a2a21
atichard <atichard.c@outlook.com>
2019-07-03 16:34:22 +0700

    "add the add button"

--- Pineapple-UI/src/app/navigation/navigation.component.ts
+++ Pineapple-UI/src/app/navigation/navigation.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent implements OnInit {
+  private sidenavWidth: 4;
+  display: any;
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
