3f4bcf36231ac28c053dc2ab90cd618eb74b8327
atichard <atichard.c@outlook.com>
2019-07-10 19:48:01 +0700

    "Add calendar"

--- Pineapple-UI/src/app/main/main.module.ts
+++ Pineapple-UI/src/app/main/main.module.ts
@@ -7,18 +7,30 @@ import { HomeComponent } from './home/home.component';
 import { MainLayoutComponent } from './main-layout/main-layout.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { DemoMaterialModule } from '../core/material-module';
-import { ReactiveFormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AddTaskComponent } from './add-task/add-task.component';
 import { EditEventComponent } from './edit-event/edit-event.component';
 import { GroupComponent } from './group/group.component';
 import { CalenderComponent } from './calender/calender.component';
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
     imports: [
         CommonModule,
         MainRoutingModule,
         DemoMaterialModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        NgbModalModule,
+        FlatpickrModule.forRoot(),
+        CalendarModule.forRoot({
+            provide: DateAdapter,
+            useFactory: adapterFactory
+        }),
+        FormsModule
     ],
     declarations: [
         AddEventComponent,
@@ -28,7 +40,7 @@ import { CalenderComponent } from './calender/calender.component';
         AddTaskComponent,
         EditEventComponent,
         GroupComponent,
-        CalenderComponent
+        CalenderComponent,
     ]
 })
 export class MainModule { }
