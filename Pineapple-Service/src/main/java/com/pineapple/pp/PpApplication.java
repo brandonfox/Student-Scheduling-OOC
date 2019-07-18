81b79e00db393735d91ec04c2d9bab3375a82935
brandonfox <brandonfox@hotmail.co.uk>
2019-07-19 06:59:20 +0700

    Cleaned up redundant imports in PpApplication

--- Pineapple-Service/src/main/java/com/pineapple/pp/PpApplication.java
+++ Pineapple-Service/src/main/java/com/pineapple/pp/PpApplication.java
@@ -1,17 +1,12 @@
 package com.pineapple.pp;
 
-import com.pineapple.pp.utils.NetConfig;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 import javax.annotation.PostConstruct;
-import java.io.BufferedReader;
-import java.net.Inet4Address;
-import java.net.URL;
+
 import java.util.TimeZone;
 
 @SpringBootApplication
