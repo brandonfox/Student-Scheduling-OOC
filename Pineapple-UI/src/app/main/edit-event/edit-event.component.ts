3bd6c9d036479168d150687ea2fc349dd1ad44d5
Sirin Chankao <sirin.cha@student.mahidol.edu>
2019-07-18 19:38:32 +0700

    Pulled event data to their respective fields in edit page

--- Pineapple-UI/src/app/main/edit-event/edit-event.component.ts
+++ Pineapple-UI/src/app/main/edit-event/edit-event.component.ts
@@ -1,50 +1,76 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 import {AuthenticationService} from '../../service/authentication.service';
 import {EventService} from '../../service/event.service';
 import {Router} from '@angular/router';
-import DateTimeFormat = Intl.DateTimeFormat;
+import {Subscription} from 'rxjs';
 
 @Component({
-  selector: 'app-edit-event',
-  templateUrl: './edit-event.component.html',
-  styleUrls: ['./edit-event.component.scss']
+    selector: 'app-edit-event',
+    templateUrl: './edit-event.component.html',
+    styleUrls: ['./edit-event.component.scss']
 })
 export class EditEventComponent implements OnInit {
-  eventForm: FormGroup;
-  loading = false;
-  retrieved = false;
-  checked = true;
-  disableSelect = new FormControl(this.checked);
-  currentEvent = this.eventService.currentEvent;
+    eventForm: FormGroup;
+    loading = false;
+    submitted = false;
+    checked = true;
+    disableSelect = new FormControl(this.checked);
+    currentEvent = this.eventService.currentEvent;
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private authService: AuthenticationService,
-    private eventService: EventService,
-    private router: Router,
-  ) { }
+    event: any = {};
+    sub: Subscription;
 
-  ngOnInit() {
-    this.eventForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      startTime: [DateTimeFormat()],
-      endTime: [DateTimeFormat()],
-      allDay: [Boolean],
-      description: [''],
-    });
-  }
+    constructor(
+        private formBuilder: FormBuilder,
+        private authService: AuthenticationService,
+        private eventService: EventService,
+        private router: Router,
+    ) { }
 
-  public onSubmit() {
-    console.log('Retrieving form');
-    this.loading = true;
-    this.retrieved = true;
-    if (this.eventForm.invalid) {
-      this.loading = false;
-      return;
+    ngOnInit() {
+        // this.sub = this.route.params.subscribe(params => {
+        //     const id = params['id'];
+        //     if (id) {
+        //         this.eventService.get(id).subscribe((event: any) => {
+        //             if (event) {
+        //                 this.event = event;
+        //                 this.event.href = event._links.self.href;
+        //                 this.giphyService.get(car.name).subscribe(url => car.giphyUrl = url);
+        //             } else {
+        //                 console.log(`Car with id '${id}' not found, returning to list`);
+        //                 this.gotoList();
+        //             }
+        //         });
+        //     }
+        // });
+        this.eventForm = this.formBuilder.group({
+            name: this.currentEvent.name,
+            startDate: this.currentEvent.startDate,
+            startTime: this.currentEvent.startTime,
+            endDate: this.currentEvent.endDate,
+            endTime: this.currentEvent.endTime,
+            location: this.currentEvent.location,
+            description: this.currentEvent.description
+        });
     }
-    this.eventService.addEvent(this.eventForm.getRawValue());
-    this.router.navigate(['main/home']);
-  }
-  get f() {return this.eventForm.controls; }
+
+    public onSubmit() {
+        console.log('Submitting form');
+        console.log(this.disableSelect.value);
+        this.eventForm.addControl(
+            'allDay',
+            new FormControl(this.disableSelect.value));
+        this.loading = true;
+        this.submitted = true;
+        if (this.eventForm.invalid) {
+            this.loading = false;
+            this.submitted = false;
+            return;
+        }
+        this.eventService.addEvent(this.eventForm.getRawValue());
+        this.router.navigate(['main/home']);
+    }
+
+    get f() {return this.eventForm.controls; }
 }
